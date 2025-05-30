f31d8bc5ce2cea3e04cb462a0c44f991274f4f6f
Nicholas Krause <nicholaskrause@alunos.utfpr.edu.br>
2025-05-30 16:23:36 -0300

    Feat: alteracao inimigo .h e .cpp

--- ProjetoSimas/inimigo.cpp
+++ ProjetoSimas/inimigo.cpp
@@ -5,25 +5,11 @@
 int Inimigo::cont_inimigo = 0;
 
 Inimigo::Inimigo() {
-	vida = 50;
-	ataque = 20;
 	id_inimigo = cont_inimigo++;
+	nivel_maldade = 0;
 }
 Inimigo::~Inimigo() {
-	vida = -1;
-	ataque = -1;
-}	
-int Inimigo::getVida() const {
-	return vida;
-}
-int Inimigo::getAtaque() const {
-	return ataque;
-}
-void Inimigo::setVida(int vida) {
-	this->vida = vida;
-}
-void Inimigo::setAtaque(int ataque) {
-	this->ataque = ataque;
+	id_inimigo = -1;
 }
 int Inimigo::getIdInimigo() const {
 	return id_inimigo;
--- ProjetoSimas/inimigo.h
+++ ProjetoSimas/inimigo.h
@@ -8,16 +8,10 @@ private:
 	static int cont_inimigo;
 	int id_inimigo;
 protected:
-	int vida;
-	int ataque;
 	int nivel_maldade;
 public:
 	Inimigo();
 	~Inimigo();
-	int getVida() const;
-	int getAtaque() const;
-	void setVida(int vida);
-	void setAtaque(int ataque);
 	int getIdInimigo() const;
 	//void salvarDataBuffer();
 	virtual void executar() = 0;
